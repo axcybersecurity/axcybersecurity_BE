13931cb4fc3ffbab386be561970c5505aa96fe36
won2eu <hasw3314@gmail.com>
2025-12-03 19:48:11 +0900

    nas file 관련 로직 모두 주석처리

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from dependencies.db import create_db_and_table
 from fastapi.middleware.cors import CORSMiddleware
 from routers.auth import router as auth_router
-from routers.files import router as files_router
+# from routers.files import router as files_router
 from routers.notice import router as notice_router
 from routers.post import router as post_router
 
@@ -24,7 +24,7 @@ app.add_middleware(
 
 # 라우터 등록
 app.include_router(auth_router)
-app.include_router(files_router)
+# app.include_router(files_router)
 app.include_router(notice_router)
 app.include_router(post_router)
 
